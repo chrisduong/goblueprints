5543fc4e8bc5e349392e55aa956a6fb08ae4c07e
Chris Duong <chrisduong83@gmail.com>
2018-11-04 17:29:22 +0800

    copy chapter1 code

---
+++ chapter2a/chat/client.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"github.com/gorilla/websocket"
+)
+
+// client represents a single chatting user.
+type client struct {
+
+	// socket is the web socket for this client.
+	socket *websocket.Conn
+
+	// send is a channel on which messages are sent.
+	send chan []byte
+
+	// room is the room this client is chatting in.
+	room *room
+}
+
+// The read function constantly check if there is any message sent by Client over websocket,
+// then it will forward to the room the client belong to
+func (c *client) read() {
+	defer c.socket.Close()
+	for {
+		_, msg, err := c.socket.ReadMessage()
+		if err != nil {
+			return
+		}
+		c.room.forward <- msg
+	}
+}
+
+// The write function check in Send queue if there is any messages
+// to send to Client over websocket
+func (c *client) write() {
+	defer c.socket.Close()
+	for msg := range c.send {
+		err := c.socket.WriteMessage(websocket.TextMessage, msg)
+		if err != nil {
+			return
+		}
+	}
+}
